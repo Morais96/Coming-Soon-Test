80f858d2bec722d162d9014a05e1dec40c7408d2
Morais96 <104147385+Morais96@users.noreply.github.com>
2023-07-10 16:46:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        var div = document.getElementById('newsletter');
+        div.style.opacity = 1;
+      }, 7000);
+});
