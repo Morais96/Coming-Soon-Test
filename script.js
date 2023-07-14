3d378b1441cc3f0b4e29152f66c733daa0b972a9
Morais96 <104147385+Morais96@users.noreply.github.com>
2023-07-14 03:13:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ window.addEventListener('load', function() {
     setTimeout(function() {
         var div = document.getElementById('newsletter');
         div.style.opacity = 1;
-      }, 7000);
+      }, 7500);
 });
